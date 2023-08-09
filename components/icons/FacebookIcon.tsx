552ae7f847f0138be8633172c479399996067658
grafek <jacek.grafender@gmail.com>
2023-08-09 21:04:56 +0200

    smaller icons on mobile

--- components/icons/FacebookIcon.tsx
+++ components/icons/FacebookIcon.tsx
@@ -7,7 +7,7 @@ function FacebookIcon() {
       strokeMiterlimit="2"
       clipRule="evenodd"
       viewBox="0 0 512 512"
-      className="h-6 w-6 fill-gray-400 transition-colors duration-300 hover:fill-red-primary md:h-8 md:w-8"
+      className="h-5 w-5 fill-gray-400 transition-colors duration-300 hover:fill-red-primary md:h-8 md:w-8"
     >
       <path d="M374.244 285.825l14.105-91.961h-88.233v-59.677c0-25.159 12.325-49.682 51.845-49.682h40.116V6.214S355.67 0 320.864 0c-72.67 0-120.165 44.042-120.165 123.775v70.089h-80.777v91.961h80.777v222.31A320.565 320.565 0 00250.408 512c16.911 0 33.511-1.324 49.708-3.865v-222.31h74.128z"></path>
     </svg>
