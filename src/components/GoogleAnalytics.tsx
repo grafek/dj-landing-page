61c538ead003c14eb6582f2af452fa9c51917a22
grafek <jacek.grafender@gmail.com>
2025-05-31 21:54:24 +0200

    fix: change error throw to console warning for missing Google Analytics ID

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -3,8 +3,8 @@ import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default function GoogleAnalytics(): JSX.Element | null {
   if (!GA_TRACKING_ID) {
-    throw new Error(
-      "Google Analytics tracking ID is not defined. Please set NEXT_PUBLIC_GOOGLE_ANALYTICS_ID in your environment variables.",
+    console.warn(
+      "Google Analytics ID is not set. Skipping Google Analytics script.",
     );
   }
 
